567f10ebbae7b87408407106bbb3c2c3ad8299b9
Azhar Waqas <azharwaqas569@gmail.com>
2019-01-26 11:38:09 +0500

    Task 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :super_admins, path: 'superAdmins'
+  get 'dashboard/home'
+  root to: 'pages#home'
+  devise_for :users, path: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
