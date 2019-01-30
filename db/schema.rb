382860924e5aa84d34e6ce502f6316da75093efb
Azhar Waqas <azharwaqas569@gmail.com>
2019-01-30 20:26:26 +0500

    resolving pg issues on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_29_094159) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
